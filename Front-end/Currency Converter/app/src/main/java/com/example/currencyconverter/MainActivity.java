817092cb6a63f7ce86bc2dd791267239e3a2c114
Jerome1710 <JeromeKabbani@hotmail.com>
2022-03-28 01:39:08 +0300

    added a calculator button and changed its layout and added two text editors

--- Front-end/Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ Front-end/Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // this hides the upper part
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide(); // this hides the action bar
+
         setContentView(R.layout.activity_main);
     }
 }
