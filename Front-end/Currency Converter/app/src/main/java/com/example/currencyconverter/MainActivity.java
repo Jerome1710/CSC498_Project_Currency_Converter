c120fa2da83a32d58f5d4d4de682f417cad1d998
Jerome1710 <JeromeKabbani@hotmail.com>
2022-03-30 19:02:17 +0300

    added the second intent from page 2 to the 1st page

--- Front-end/Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ Front-end/Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void getToCalculator (View view){
+    public void goToCalculator (View view){
         Intent obj = new Intent(getApplicationContext(), MainActivity2.class);
         startActivity(obj);
     }
