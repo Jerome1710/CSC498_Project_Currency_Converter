08cf7c8d5035f483bed1e87dc3e69fdfcd1cf764
Jerome1710 <JeromeKabbani@hotmail.com>
2022-04-03 02:07:26 +0300

    wrote the buy code

--- Front-end/Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ Front-end/Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -7,6 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +31,69 @@ public class MainActivity extends AppCompatActivity {
         //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         getSupportActionBar().hide(); // this hides the action bar
-
         setContentView(R.layout.activity_main);
+
+        String url = "http://192.168.0.101/Currency_Converter/get.php";
+
+        DownloadTask task = new DownloadTask();
+        task.execute(url);
+
     }
 
     public void goToCalculator (View view){
         Intent obj = new Intent(getApplicationContext(), MainActivity2.class);
         startActivity(obj);
     }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls){
+            String result = "";
+            URL url;
+            HttpURLConnection http;
+
+            try{
+                url = new URL(urls[0]);
+                http = (HttpURLConnection) url.openConnection();
+
+                InputStream in = http.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while( data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+
+            return result;
+        }
+
+
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+
+            try{
+                JSONObject json = new JSONObject(s);
+                String buy = json.getString("buy_rate");
+                String sell = json.getString("sell_rate");
+                Log.i("buy_rate", buy);
+                Log.i("sell_rate", sell);
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
 }
+
+
+
