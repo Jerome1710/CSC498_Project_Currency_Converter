3f7f20ac64811491771e60bacf264805701bb597
Jerome1710 <JeromeKabbani@hotmail.com>
2022-04-03 19:54:10 +0300

    created the refresh method

--- Front-end/Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ Front-end/Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -5,22 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.json.JSONObject;
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView buy;
+    TextView sell;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide(); // this hides the action bar
         setContentView(R.layout.activity_main);
 
+        buy = findViewById(R.id.textView);
+        sell = findViewById(R.id.textView3);
+
         String url = "http://192.168.0.101/Currency_Converter/get.php";
 
         DownloadTask task = new DownloadTask();
@@ -43,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     public void goToCalculator (View view){
         Intent obj = new Intent(getApplicationContext(), MainActivity2.class);
         startActivity(obj);
+
     }
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
@@ -64,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
                     char current = (char) data;
                     result += current;
                     data = reader.read();
-
                 }
             }catch(Exception e){
                 e.printStackTrace();
@@ -80,10 +81,10 @@ public class MainActivity extends AppCompatActivity {
 
             try{
                 JSONObject json = new JSONObject(s);
-                String buy = json.getString("buy_rate");
-                String sell = json.getString("sell_rate");
-                Log.i("buy_rate", buy);
-                Log.i("sell_rate", sell);
+                String buy_rate = json.getString("buy_rate");
+                String sell_rate = json.getString("sell_rate");
+                buy.setText("Buy 1 USD at " + buy_rate + " LBP");
+                sell.setText("Sell 1 USD at " + sell_rate + " LBP");
 
             }catch(Exception e){
                 e.printStackTrace();
@@ -92,34 +93,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void refresh(View view){
+        String url = "http://192.168.0.101/Currency_Converter/get.php";
+
+        DownloadTask task = new DownloadTask();
+        task.execute(url);
+    }
 
 }
-
-    //
-    URL url = new URL ("https://reqres.in/api/users");
-    //
-    HttpURLConnection con = (HttpURLConnection)url.openConnection();
-    //
-    con.setRequestMethod("POST");
-    //
-    con.setRequestProperty("Content-Type", "application/json; utf-8");
-    //
-    con.setRequestProperty("Accept", "application/json");
-    //
-    con.setDoOutput(true);
-    //
-    String jsonInputString = "{"name": "Upendra", "job": "Programmer"}";
-    //
-    try(OutputStream os = con.getOutputStream()) {
-        byte[] input = jsonInputString.getBytes("utf-8");
-        os.write(input, 0, input.length);
-    }
-    //
-    try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
-        StringBuilder response = new StringBuilder();
-        String responseLine = null;
-        while ((responseLine = br.readLine()) != null) {
-            response.append(responseLine.trim());
-        }
-        System.out.println(response.toString());
-    }
