18118ea79f84ea430699b9746d58ad6e86651a97
Jerome1710 <JeromeKabbani@hotmail.com>
2022-04-03 14:25:51 +0300

    added the get rate code

---
+++ Back-end/get_rate.php
@@ -0,0 +1,30 @@
+<?php
+
+include('ConnecttoDb\my_db.php');
+
+$query = $mysqli->prepare("SELECT count(id) FROM rates;");
+$query->execute();
+$result = $query->get_result();
+$row = mysqli_fetch_row($result);
+$counter = $row[0]; 
+
+
+$query = $mysqli->prepare("SELECT buy_rate, sell_rate FROM rates WHERE id = $counter;");
+$query->execute();
+$result = $query->get_result();
+
+$buy_sell = [];
+
+while($rates = $result->fetch_assoc()){
+    $buy_sell[] = $rates;
+}
+
+$json_buy_sell = json_encode($buy_sell[0]);
+echo $json_buy_sell;
+
+
+
+?>
+
+
+
--- Front-end/Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ Front-end/Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -95,5 +95,31 @@ public class MainActivity extends AppCompatActivity {
 
 }
 
-
-
+    //
+    URL url = new URL ("https://reqres.in/api/users");
+    //
+    HttpURLConnection con = (HttpURLConnection)url.openConnection();
+    //
+    con.setRequestMethod("POST");
+    //
+    con.setRequestProperty("Content-Type", "application/json; utf-8");
+    //
+    con.setRequestProperty("Accept", "application/json");
+    //
+    con.setDoOutput(true);
+    //
+    String jsonInputString = "{"name": "Upendra", "job": "Programmer"}";
+    //
+    try(OutputStream os = con.getOutputStream()) {
+        byte[] input = jsonInputString.getBytes("utf-8");
+        os.write(input, 0, input.length);
+    }
+    //
+    try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+        StringBuilder response = new StringBuilder();
+        String responseLine = null;
+        while ((responseLine = br.readLine()) != null) {
+            response.append(responseLine.trim());
+        }
+        System.out.println(response.toString());
+    }
