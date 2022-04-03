de5144549a555bbd5b55ad5268faabd6f1576894
Jerome1710 <JeromeKabbani@hotmail.com>
2022-04-03 22:15:17 +0300

    added comments to android studio .java class

--- Back-end/Currency_Converter/Buy.php
+++ Back-end/Currency_Converter/Buy.php
@@ -1,7 +1,7 @@
 <?php
-$url = 'https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233019';
+$url = 'https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t20224319';
 $json = file_get_contents($url);
 $jo = json_decode($json);
 $length = count($jo->buy);
-echo $jo->buy[$length - 1][1];
+$updated_buy = $jo->buy[$length - 1][1];
 ?>
--- Back-end/Currency_Converter/Sell.php
+++ Back-end/Currency_Converter/Sell.php
@@ -1,7 +1,7 @@
 <?php
-$url = 'https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233019';
+$url = 'https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t20224319';
 $json = file_get_contents($url);
 $jo = json_decode($json);
 $length = count($jo->sell);
-echo $jo->sell[$length - 1][1];
+$updated_sell = $jo->sell[$length - 1][1];
 ?>
